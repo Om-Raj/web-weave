73ccc3118778ae5ba76ae564e5e07a82604ae90e
Om Raj <34886077+Om-Raj@users.noreply.github.com>
2025-06-27 21:10:56 +0530

    Tested gemini using a simple input prompt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,24 @@
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
+import { useState } from "react";
 
 export default function Home() {
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess: () => {
       toast.success("Background job invoked");
-    },
-    onError: () => {
-      toast.error("Failed to invoke background job");
-    },
+    }
   }));
+
+  const [value, setValue] = useState("");
+
   return (
     <div>
-      <Button onClick={() => invoke.mutate({ text: "Om" })}>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button onClick={() => invoke.mutate({ value: value })}>
         Invoke Background Job
       </Button>
     </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,25 @@
+import { Agent, gemini, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer. You write the clean, maintainable code. You write simple Next.js & React snippets.",
+      model: gemini({ model: "gemini-2.5-flash"}),
+    });
+
+    // Run the agent with an input.  This automatically uses steps
+    // to call your AI model.
+    const { output } = await codeAgent.run(
+      `Code the following snippet: ${event.data.value}`
+    );
+
+    return { output };
   },
 );
