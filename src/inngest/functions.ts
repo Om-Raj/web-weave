612cb7ecb7aa66845db130ecb565856754c26ce8
Om Raj <34886077+Om-Raj@users.noreply.github.com>
2025-06-27 23:35:44 +0530

    Deployed Next.js app in E2B Sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,17 @@
-import { Agent, gemini, createAgent } from "@inngest/agent-kit";
+import { gemini, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
 
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("web-weave-test-2");
+      return sandbox.sandboxId;
+    });
 
     // Create a new agent with a system prompt (you can add optional tools, too)
     const codeAgent = createAgent({
@@ -20,6 +26,13 @@ export const helloWorld = inngest.createFunction(
       `Code the following snippet: ${event.data.value}`
     );
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+        const sandbox = await getSandbox(sandboxId);
+        const host = sandbox.getHost(3000);
+        return `http://${host}`;
+      }
+    );
+
+    return { output, sandboxUrl };
   },
 );
