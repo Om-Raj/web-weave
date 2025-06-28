db0029645328c50b0b4b85b3fa3cbce7609f398c
Om Raj <34886077+Om-Raj@users.noreply.github.com>
2025-06-28 20:02:45 +0530

    Created chat UI and form

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,3 +1,8 @@
+import { ProjectView } from "@/modules/projects/ui/project-views";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
 interface Props {
     params: Promise<{
         projectId: string;
@@ -7,10 +12,16 @@ interface Props {
 const Page = async ({ params }: Props) => {
     const { projectId } = await params;
 
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(trpc.projects.getOne.queryOptions({ id: projectId }));
+    void queryClient.prefetchQuery(trpc.messages.getMany.queryOptions({ projectId }));
+
     return (
-        <div>
-            <h1>Project {projectId}</h1>
-        </div>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<div>Loading...</div>}>
+                <ProjectView projectId={projectId} />
+            </Suspense>
+        </HydrationBoundary>
     );
 };
 
